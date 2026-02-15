b1dbdf73b69e01cfbe5fecc7e61f4437f745c826
Dhruv Rai <iideaconii@Dhruvs-MacBook-Air.local>
2026-02-16 04:07:47 +0530

    finale

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8800",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export const addToMyBooks = (bookId) => api.post("/user-books", { bookId });
